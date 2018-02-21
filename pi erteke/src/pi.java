c34b57d853dc41ff079823fb542a6347ed96a386
szucs28 <szucs28@iit342.iit.uni-miskolc.hu>
2018-02-21 13:39:45 +0100

    Leibniz

--- pi erteke/src/pi.java
+++ pi erteke/src/pi.java
@@ -13,5 +13,22 @@ public class pi {
 		System.out.println("Wallis-formula");
 		System.out.println("pi/2 erteke: " + pi);
 		System.out.println("pi: " + pi * 2);
+	
+
+
+		//Leibniz
+		pi = 0;
+		darab *= 4;
+		int elojel = 1;
+		
+		for (int i = 1; i <= darab; i+=2) {
+			pi += elojel * 1.0 / i;
+			elojel *= -1;
+		}
+	
+		System.out.println("Leibniz-féle sor");
+		System.out.println("pi/4 erteke: " + pi);
+		System.out.println("pi: " + pi * 4);
+		
 	}
 }
