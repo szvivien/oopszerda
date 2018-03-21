1ce5bf18ef52bd146561656bfec6b4251ff95f30
szucs28 <szucs28@iit433.iit.uni-miskolc.hu>
2018-03-21 13:40:42 +0100

    szam

---
+++ Szam_csomagok/src/sajat/masodik/Futtathato.java
@@ -0,0 +1,13 @@
+package sajat.masodik;
+
+import sajat.elso.Szam;
+
+public class Futtathato {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Szam szam = new Szam(Math.random());
+
+	}
+
+}
